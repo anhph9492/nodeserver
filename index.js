558949be344b9bf163cbde813252b76608406961
Anh Hoàng <57165054+anhph9492@users.noreply.github.com>
2024-03-25 09:44:36 +0000

    a

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 // index.js
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 4000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://anhph9492mongodbtest:forever9492@cluster0.xvgzewq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const cors = require("cors");
+app.use(cors());
 
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/graphql-demo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./src/graphql/schema.js");
+const resolvers = require("./src/graphql/Resolvers.js");
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
+  }),
+);
 // const redis = require('redis');
 // const client = redis.createClient({
 //   password: 'mRY81VkGg3AHJe4EBMhdD7axWLQqy0Dk',
