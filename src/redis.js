f9b4f1ca92d554e83ab68b91cd37d1ea5fc7177e
Anh Hoàng <57165054+anhph9492@users.noreply.github.com>
2024-03-25 09:06:03 +0000

    ok

---
+++ src/redis.js
@@ -0,0 +1,16 @@
+// const redis = require('redis');
+// const client = redis.createClient({
+//   password: 'mRY81VkGg3AHJe4EBMhdD7axWLQqy0Dk',
+//   socket: {
+//       host: 'redis-19558.c1.asia-northeast1-1.gce.cloud.redislabs.com',
+//       port: 19558
+//   }
+// });
+// client.on('error', err => console.log('Redis Client Error', err));
+// client.connect()
+// .then(res => {
+//   console.log('redis connection successfully - ')
+// })
+// .catch(err => {
+//   console.log('redis connection falure - ')
+// });
